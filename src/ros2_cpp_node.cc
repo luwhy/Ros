57f510b5c8150a4e530c9bc5126a2ab1a6dad542
luwhy <1023412173@qq.com>
2026-04-18 12:17:26 +0800

    first-commit

---
+++ src/ros2_cpp_node.cc
@@ -0,0 +1,12 @@
+
+#include<iostream>
+#include<rclcpp/rclcpp.hpp>
+#include<memory>
+int main(int argc,char** argv){
+    rclcpp::init(argc,argv);
+    auto node=std::make_shared<rclcpp::Node>("cpp_node");//应该是个节点名
+    RCLCPP_INFO(node->get_logger(),"hello world");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
