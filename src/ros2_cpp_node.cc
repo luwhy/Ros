9e96a4fc6de6e23dba0e96ed6632093b168faece
luwhy <1023412173@qq.com>
2026-04-20 23:39:24 +0800

    第二次提交，开始创建功能包

--- src/ros2_cpp_node.cc
+++ src/ros2_cpp_node.cc
@@ -6,6 +6,7 @@ int main(int argc,char** argv){
     rclcpp::init(argc,argv);
     auto node=std::make_shared<rclcpp::Node>("cpp_node");//应该是个节点名
     RCLCPP_INFO(node->get_logger(),"hello world");
+    //循环检测节点内容
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
